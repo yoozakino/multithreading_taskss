f836c2257c1d4f402633b1a70f0c189e7e963664
yoozakino <162565028+yoozakino@users.noreply.github.com>
2025-02-13 12:42:47 +0300

    Initial commit

--- src/4.kt
+++ src/4.kt
@@ -23,12 +23,32 @@ suspend fun main()= coroutineScope {
             remittance(account3, account1, account3Balance, account1Balance, 13000)
         }
     }
+
+    val job3 = launch {
+        synchronized(lock) {
+            remittance(account2, account3, account2Balance, account3Balance, 15000)
+        }
+    }
+
+    val job4 = launch {
+        synchronized(lock) {
+            remittance(account1, account3, account1Balance, account3Balance, 12000)
+        }
+    }
 }
 
 fun remittance(sender:String, recipient:String, senderBalance:Int, recipientBalance:Int, amount: Int) {
-    val newSenderBalance = senderBalance - amount
-    val newRecipientBalance = recipientBalance + amount
 
-    println("Средства были успешно переведены с аккаунта $sender на аккаунт $recipient");
-    println("Баланс отправителя $sender: $newSenderBalance | Баланс получателя $recipient: $newRecipientBalance")
+    if (amount > senderBalance) {
+        println("На балансе $sender недостаточно средств для перевода")
+    }
+
+    else {
+        val newSenderBalance = senderBalance - amount
+        val newRecipientBalance = recipientBalance + amount
+
+        println("Средства были успешно переведены с аккаунта $sender на аккаунт $recipient");
+        println("Баланс отправителя $sender: $newSenderBalance | Баланс получателя $recipient: $newRecipientBalance")
+    }
+
 }
